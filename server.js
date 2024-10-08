2270c66c8fb8dc8bde7864736c669047135149e3
andrewindeche <indecheandrew@gmail.com>
2024-10-08 13:13:26 +0300

    update routes

--- server.js
+++ server.js
@@ -8,11 +8,30 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.listen (PORT, () => {
-    console.log(`server is listening on http://localhost:${PORT}`)
-})
+app.use(express.json());
 
 app.get('/api/quotes/random', (req,res) => {
     const randomQuote = getRandomElement(quotes);
-    res.send({ quote : randomQuote });
+    res.json({ quote : randomQuote });
+});
+
+app.get('/api/quotes',(req,res) => {
+    const { person } = req.query;
+
+    const filteredQuotes = person ? quotes.filter(quote => quote.author === person)
+         : quotes;
+         res.send({quotes: filteredQuotes});
+});
+
+app.post("/api/quotes", (req, res) => {
+    const { quote, person } = req.query;
+    const newQuote = { quote, person };
+  
+    !quote || !person
+      ? res.status(400).send()
+      : (quotes.push(newQuote).status(201).json());
+  });
+
+  app.listen (PORT, () => {
+    console.log(`server is listening on http://localhost:${PORT}`)
 })
